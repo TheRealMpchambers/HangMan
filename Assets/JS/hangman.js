da5a114235b39f28453730aecd9152436e667ad4
Mikkel Phillip Chambers <32176553+TheRealMpchambers@users.noreply.github.com>
2018-10-08 17:59:51 -0500

    first

---
+++ Assets/JS/hangman.js
@@ -0,0 +1,16 @@
+//Create an array of words
+const word = ['fat', 'lazy', 'hungry', 'zealot', 'bombarding'];
+// Choose word randomly
+let randNum = Math.floor(Math.random() * words.length);
+let choosenWord = word[randNum];
+let underScore = [];
+//Create underscores based on length of word
+let generateUnderscore = () => {
+    for(let i = 0; i < choosenWord.length; i++) {
+
+    }
+}
+//Get users guess
+//Check if guess is right
+//if right push to right array
+//if wrong push to wrong array
